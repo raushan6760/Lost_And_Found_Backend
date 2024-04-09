d019d8c5ba1dbc90490c1cb1aac95f9503d51e2a
unknown <lcs2021032@iiitl.ac.in>
2024-04-09 16:05:15 +0530

    change server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import app from "./app.js";
 import cloudinary from "cloudinary";
-
+const PORT =process.env.PORT || 4000
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
   api_key: process.env.CLOUDINARY_CLIENT_API,
